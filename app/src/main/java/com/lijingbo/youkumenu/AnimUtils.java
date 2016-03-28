c74f05858d4aa3e9288a31e1539352185117cac7
jingbolee <jingbolee@foxmail.com>
2016-03-28 13:05:20 +0800

    完成优酷menu的逻辑

--- app/src/main/java/com/lijingbo/youkumenu/AnimUtils.java
+++ app/src/main/java/com/lijingbo/youkumenu/AnimUtils.java
@@ -1,30 +1,60 @@
 package com.lijingbo.youkumenu;
 
-import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.widget.RelativeLayout;
 
-/**
- * @FileName: com.lijingbo.youkumenu.AnimUtils.java
- * @Author: Li Jingbo
- * @Date: 2016-03-28 11:27
- * @Version V1.0 <描述当前版本功能>
- */
+
 public class AnimUtils {
     private static final String TAG = "AnimUtils";
+    public static int animCount = 0;
 
     //关闭动画效果
-    public static void closeMenu(View view) {
+    public static void closeMenu(RelativeLayout view, long startOffset) {
+        //view关闭动画以后，所有的子view的enable为true
+        for ( int i = 0; i < view.getChildCount(); i++ ) {
+            view.getChildAt(i).setEnabled(false);
+        }
+
         RotateAnimation animation = new RotateAnimation(0, -180, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 1f);
-        animation.setDuration(200);
+        animation.setDuration(400);
+        animation.setStartOffset(startOffset);
         animation.setFillAfter(true);
         view.startAnimation(animation);
+        animation.setAnimationListener(new AnimListener());
     }
 
     //开始动画效果
-    public static void startMenu(View view) {
+    public static void startMenu(RelativeLayout view, long startOffset) {
+        //view显示动画以后，所有的子view的enable为true
+        for ( int i = 0; i < view.getChildCount(); i++ ) {
+            view.getChildAt(i).setEnabled(true);
+        }
         RotateAnimation animation = new RotateAnimation(-180, 0, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 1f);
         animation.setFillAfter(true);
-        animation.setDuration(200);
+        animation.setDuration(400);
+        animation.setStartOffset(startOffset);
         view.startAnimation(animation);
+        animation.setAnimationListener(new AnimListener());
+    }
+
+    static class AnimListener implements Animation.AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+            //有动画执行，++
+            animCount++;
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            //没有动画执行，--
+            animCount--;
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
--- app/src/main/java/com/lijingbo/youkumenu/MainActivity.java
+++ app/src/main/java/com/lijingbo/youkumenu/MainActivity.java
@@ -2,6 +2,7 @@ package com.lijingbo.youkumenu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -11,18 +12,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView iv_home, iv_menu;
     private boolean isShowLevel2 = true;
     private boolean isShowLevel3 = true;
+    private boolean isShowLevel1 = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
         initListener();
+
     }
 
     private void initView() {
         setContentView(R.layout.activity_main);
         rl_level2 = (RelativeLayout) findViewById(R.id.rl_level2);
         rl_level3 = (RelativeLayout) findViewById(R.id.rl_level3);
+        rl_level1 = (RelativeLayout) findViewById(R.id.rl_level1);
         iv_home = (ImageView) findViewById(R.id.iv_home);
         iv_menu = (ImageView) findViewById(R.id.iv_menu);
     }
@@ -36,27 +40,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch ( v.getId() ) {
             case R.id.iv_home:
+                //判断是否有动画在执行
+                if ( AnimUtils.animCount != 0 ) {
+                    break;
+                }
                 if ( isShowLevel2 ) {
+                    int startOffset = 0;
+                    if ( isShowLevel3 ) {
+                        AnimUtils.closeMenu(rl_level3, startOffset);
+                        isShowLevel3 = false;
+                        startOffset += 200;
+                    }
                     //关闭二级菜单
-                    AnimUtils.closeMenu(rl_level2);
+                    AnimUtils.closeMenu(rl_level2, startOffset);
                 } else {
                     //显示二级菜单
-                    AnimUtils.startMenu(rl_level2);
+                    AnimUtils.startMenu(rl_level2, 0);
                 }
                 isShowLevel2 = !isShowLevel2;
                 break;
 
             case R.id.iv_menu:
+                //判断是否有动画在执行
+                if ( AnimUtils.animCount != 0 ) {
+                    break;
+                }
                 if ( isShowLevel3 ) {
                     //关闭三级次菜单
-                    AnimUtils.closeMenu(rl_level3);
+                    AnimUtils.closeMenu(rl_level3, 0);
                 } else {
                     //显示三级菜单
-                    AnimUtils.startMenu(rl_level3);
+                    AnimUtils.startMenu(rl_level3, 0);
                 }
                 isShowLevel3 = !isShowLevel3;
 
                 break;
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch ( keyCode ) {
+            //监听menu键
+            case KeyEvent.KEYCODE_MENU:
+                //判断是否有动画在执行
+                if ( AnimUtils.animCount != 0 ) {
+                    break;
+                }
+                if ( isShowLevel1 ) {
+                    int startOffset = 0;
+                    if ( isShowLevel2 ) {
+                        if ( isShowLevel3 ) {
+                            AnimUtils.closeMenu(rl_level3, startOffset);
+                            startOffset += 200;
+                            isShowLevel3 = false;
+                        }
+                        AnimUtils.closeMenu(rl_level2, startOffset);
+                        startOffset += 200;
+                        isShowLevel2 = false;
+                    }
+                    AnimUtils.closeMenu(rl_level1, startOffset);
+                    isShowLevel1 = false;
+                } else {
+                    AnimUtils.startMenu(rl_level1, 0);
+                    AnimUtils.startMenu(rl_level2, 200);
+                    AnimUtils.startMenu(rl_level3, 400);
+                    isShowLevel1 = true;
+                    isShowLevel2 = true;
+                    isShowLevel3 = true;
+                }
+                break;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
